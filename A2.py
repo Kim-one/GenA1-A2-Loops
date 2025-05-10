244abce7a0400ccbe90d0ff48a7fe2ff1acc7f26
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-05-10 17:09:56 -0300

    first commit

---
+++ A2.py
@@ -0,0 +1,29 @@
+# Assignment 2:
+
+# Task 1: Counting down using loops
+# Prompt user for a starting number
+starting_num = int(input("Enter starting number: "))
+
+# Loop prints the starting number then continues to decrease until it reaches 1. 
+while starting_num>0:
+    print(starting_num,end=" ")
+    starting_num-=1
+
+print("Blast off! 🚀")
+
+# Task 2:   Multiplication using loops
+num = int(input("Enter a number: "))
+for i in range(1,11):
+    print(f"{num}x{i}=", i*num)
+
+# Task 3: Find the Factorial
+# Get user input 
+num_fact = int(input("Enter a number: "))
+
+# initialize factorial to 1
+fact = 1
+# Loop to calculate the factorial. Multiplies each number 'i' by the factorial
+for i in range(1, num_fact+1):
+    fact*=i
+
+print(f"The factorial of {num_fact} is ", fact)
